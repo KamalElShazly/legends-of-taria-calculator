59f82f220dcad6a02b952666e4eaf5ae857b2c32
Kamal El-Shazly <kamal.elshazly@izam.co>
2026-02-28 14:29:41 +0200

    feat: add smelting again
    This reverts commit 7e3b2f8b13f884ad44847b9987d644cb20b11b30.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from "./components/Footer";
 import Woodcutting from "./tabs/Woodcutting";
 import Mining from "./tabs/Mining";
 import Fishing from "./tabs/Fishing";
+import Smelting from "./tabs/Smelting";
 import Smithing from "./tabs/Smithing";
 import Cooking from "./tabs/Cooking";
 
@@ -26,6 +27,7 @@ const App = () => {
               <Tab label="Mining" component={Link} to="/mining" value="/mining" />
               <Tab label="Woodcutting" component={Link} to="/woodcutting" value="/woodcutting" />
               <Tab label="Fishing" component={Link} to="/fishing" value="/fishing" />
+              <Tab label="Smelting" component={Link} to="/smelting" value="/smelting" />
               <Tab label="Smithing" component={Link} to="/smithing" value="/smithing" />
               <Tab label="Cooking" component={Link} to="/cooking" value="/cooking" />
             </Tabs>
@@ -37,6 +39,7 @@ const App = () => {
         <Route path="mining" element={<Mining />} />
         <Route path="woodcutting" element={<Woodcutting />} />
         <Route path="fishing" element={<Fishing />} />
+        <Route path="smelting" element={<Smelting />} />
         <Route path="smithing" element={<Smithing />} />
         <Route path="cooking" element={<Cooking />} />
       </Routes>
